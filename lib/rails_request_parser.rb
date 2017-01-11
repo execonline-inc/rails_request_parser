437bcd8590fb561b13b23cf712205579adb61cc4
Joe Gesualdo <joegesualdo@gmail.com>
2017-01-11 17:32:02 -0500

    Clean code

--- lib/rails_request_parser.rb
+++ lib/rails_request_parser.rb
@@ -18,12 +18,3 @@ class RailsRequestParser
     UserAgentParser.parse(@request.user_agent).os.to_s.include? windows
   end
 end
-
-# firefox_user_agent =  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:43.0) Gecko/20100101 Firefox/43.0"
-# chrome_user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36"
-#
-# Request = Struct.new(:user_agent)
-# request = Request.new(chrome_user_agent)
-# puts request
-# request_parser = RailsRequestParser.new(request)
-# puts request_parser.is_chrome_browser?
--- lib/rails_request_parser/version.rb
+++ lib/rails_request_parser/version.rb
@@ -1,3 +1,3 @@
 class RailsRequestParser
-  VERSION = "0.1.2"
+  VERSION = "0.1.3"
 end
